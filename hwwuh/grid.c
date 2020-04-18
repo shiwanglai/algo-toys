13d27f42beafba290b012a1e0e65cd3741f92535
Roger Shi <swlhmq@163.com>
2020-04-18 10:49:14 +0800

    dfs

--- hwwuh/grid.c
+++ hwwuh/grid.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+#define GRID_ROWS    100
+#define GRID_COLS    100
+
 /*
  * function divide and conquer
  *
@@ -138,10 +141,82 @@ int f_dc_cache(int x, int y)
     return cache[x][y];
 }
 
+/*
+ * dfs, depth first search
+ *
+ * It's a way of step and options.
+ * The step is a step in depth.
+ * The options is the choices after the step.
+ * Commonly there are 4 options, namely 4 directions(right/down/left/up).
+ *
+ * Different from the former topic, which always asked a nearest way,
+ * this topic only requests that you not go the same node.
+ * A kidding saying is to send a fairy home :)
+ *
+ * We defined a step function which return the number of ways home.
+ * The parameter x, y is start points.
+ * The parameter tx, ty is the home/target point.
+ *
+ *   0    1    2    3
+ * 0 %----$----*----*
+ *   |    |    |    |
+ * 1 @----*----*----*
+ *   |    |    |    |
+ * 2 *----*----*----#
+ *
+ * The start point is %, target is #
+ * A first step is to %, its options is $ and @
+ */
+static int visited[GRID_ROWS][GRID_COLS];
+int step(int x, int y, int tx, int ty)
+{
+	/*
+	 * match
+	 */
+	if (x == tx && y == ty) {
+		/* reach the target, one way */
+		return 1;
+	}
+
+	/*
+	 * out of bounds
+	 */
+	if (x < 0 || y < 0 || x > tx || y > ty) {
+		/* out of the scope, no way */
+		return 0;
+	}
+
+	/*
+	 * options
+	 */
+
+	/* check visted */
+	if (visited[x][y]) {
+		/* it's a rule, no visit a node twice */
+		return 0;
+	}
+
+	/* tag visited */
+	visited[x][y] = 1;
+
+	int sum = 0;
+	sum += step(x + 1, y, tx, ty); /* right */
+	sum += step(x, y + 1, tx, ty); /* down */
+	sum += step(x - 1, y, tx, ty); /* left */
+	sum += step(x, y - 1, tx, ty); /* up */
+
+	/* 4 directions finished, we've gone all the ways here
+	 * maybe we've reach the target
+	 * maybe not, however now we should leave the node */
+	visited[x][y] = 0;
+
+	return sum;
+}
+
 int main()
 {
-    int x = 15;
-    int y = 14;
+    int x = 4; //15;
+    int y = 3; //14;
  
     printf("f_dc(%d, %d) = %d\n", x, y, f_dc(x, y));
     printf("f_dp(%d, %d) = %d\n", x, y, f_dp(x, y));
@@ -152,6 +227,11 @@ int main()
     count_cache = 0;
     printf("f_dc_cache(%d, %d) = %d\n", x, y, f_dc_cache(x, y));
     printf("f_dc_cache(%d, %d) count = %d\n", x, y, count_cache);
- 
+
+    /* dfs */
+    int sx = 0, sy = 0; /* start */
+    int tx = 2, ty = 2; /* target 2,2 is 12 ways */
+    printf("step(%d, %d, %d, %d) = %d\n", sx, sy, tx, ty, step(sx, sy, tx, ty));
+
     return 0;
 }
